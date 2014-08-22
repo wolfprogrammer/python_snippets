0c226f4bb21c3de3b80548bcae469c9e406550b5
wolfprogrammer <ghost@gmail.com>
2014-08-22 11:49:37 -0300

    added get_resource_file to resource_path.py

--- resource_path.py
+++ resource_path.py
@@ -7,17 +7,6 @@ reused.
 
 """
 
-def resource_path(filename):
-    """
-    :param filename: (str)  Name of file in same directory of script
-    Returns absolute path to file in same directory that this function
-    is being called.
-    """
-
-    import os
-    import inspect
-    return os.path.join( os.path.dirname(os.path.abspath(inspect.stack()[1][1])), filename)
-
 def this():
     """
     Returns the absolute path to the script that calls this function
@@ -34,4 +23,40 @@ def this_dir():
     import inspect
     return os.path.dirname(os.path.abspath(inspect.stack()[1][1]))
 
+def resource_path(filename):
+    """
+    :param filename: (str)  Name of file in same directory of script
+    Returns absolute path to file in same directory that this function
+    is being called.
+    """
 
+    import os
+    import inspect
+    return os.path.join( os.path.dirname(os.path.abspath(inspect.stack()[1][1])), filename)
+
+def get_resource_file(filen):
+    """
+    :param filen: (str) File name of resource file
+    :return:
+
+    Return content of file in same directory of the script calling
+    this routine or inside the zip file if the script is imported
+    from a zip file ( Python egg file).
+
+    """
+    import zipfile
+    import os
+    import inspect
+    this_directory = os.path.dirname(os.path.abspath(inspect.stack()[1][1]))
+    #logger.debug("Getting resource file %s" % filen)
+
+    if zipfile.is_zipfile(this_directory):
+       #logger.debug("ZIP FILE")
+       zf = zipfile.ZipFile(this_directory)
+       data = zf.read(filen)
+
+    else:
+       #logger.debug("NOT ZIP FILE")
+       data = open(os.path.join(this_directory, filen)).read()
+
+    return data
